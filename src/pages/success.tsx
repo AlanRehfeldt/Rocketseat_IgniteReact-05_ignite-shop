0e3e584d1a49cad32ce26282f9f14f55e2fa2be1
Alan Rehfeldt <alanrehfeldt@gmail.com>
2024-03-30 22:31:59 -0300

    trabalhando com SEO

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -4,6 +4,7 @@ import { GetServerSideProps } from "next";
 import { stripe } from "../lib/stripe";
 import Stripe from "stripe";
 import Image from "next/image";
+import Head from "next/head";
 
 export interface SuccessProps {
   customerName: string;
@@ -15,21 +16,27 @@ export interface SuccessProps {
 
 export default function Success({ customerName, product }: SuccessProps) {
   return (
-    <SuccessContainer>
-      <h1>Compra efetuada</h1>
+    <>
+      <Head>
+        <title>Compra efetuada | Ignite Shop</title>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <SuccessContainer>
+        <h1>Compra efetuada</h1>
 
-      <ImageContainer>
-        <Image src={product.imageUrl} width={120} height={110} alt={`Imagem do produto ${product.name}`} />
-      </ImageContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={120} height={110} alt={`Imagem do produto ${product.name}`} />
+        </ImageContainer>
 
-      <p>
-        Uhuul, <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
-      </p>
+        <p>
+          Uhuul, <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+        </p>
 
-      <Link href='/' >
-        Voltar ao catálago
-      </Link>
-    </SuccessContainer>
+        <Link href='/' >
+          Voltar ao catálago
+        </Link>
+      </SuccessContainer>
+    </>
   )
 }
 
